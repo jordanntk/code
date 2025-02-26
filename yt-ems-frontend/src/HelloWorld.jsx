74c9ca7a9d246c915903947b685c4bcc86ad5e73
JORDAN NG TENG KIAT (HUANG DINGJIE) <131585577+jordanntk@users.noreply.github.com>
2025-02-27 02:15:38 +0800

    recommit to see if deleted

--- yt-ems-frontend/src/HelloWorld.jsx
+++
@@ -1,4 +0,0 @@
-function HelloWorld() {
-  return <h1 className="text-center">Hello World na</h1>;
-}
-export default HelloWorld
--- yt-ems-tutorial/src/main/java/com/example/yt_ems_tutorial/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.example.yt_ems_tutorial.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.yt_ems_tutorial.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
